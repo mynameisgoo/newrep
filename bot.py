256ebde4b7aa60517622ef52b487098add79ea35
mynameisgoo <40825601+mynameisgoo@users.noreply.github.com>
2018-08-01 20:23:49 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,45 +5,64 @@ from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start'])
-def start(message):
+def keyboard1():
+    """
+    клавиатура со старта
+    :return: кнопки "Просмотреть заявки", "Создать заявку"
+    """
     keyboard = types.ReplyKeyboardMarkup(True, True)
     keyboard.row('Просмотреть заявки', 'Создать заявку')
-    bot.send_message(message.chat.id, 'Привет, я бот, можешь смотреть или создать заявку', reply_markup=keyboard)
+    global key1
+    key1 = keyboard
 
-@bot.message_handler(func=lambda message: message.text == 'Просмотреть заявки', content_types=['text'])
-def orders(message):
+def keyboard2():
+    """
+    :return: Вернуться, Следующая страница
+    """
     keyboard = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Вернуться', callback_data='quit')
     btn2 = types.InlineKeyboardButton('Cледующая страница', callback_data='next')
     keyboard.add(btn1, btn2)
-    bot.send_message(message.chat.id, 'Заявки:', reply_markup=keyboard)
+    global key2
+    key2 = keyboard
 
-@bot.callback_query_handler(func=lambda call: call.data == 'quit')
-def start(call):
-    keyboard = types.ReplyKeyboardMarkup(True, True)
-    keyboard.row('Просмотреть заявки', 'Создать заявку')
-    bot.send_message(chat_id=call.message.chat.id, text='Привет, я бот, можешь смотреть или создать заявку', reply_markup=keyboard)
-
-@bot.callback_query_handler(func=lambda call: call.data == 'next')
-def callback_next(call):
-    # num = num + 1
+def keyboard3():
+    """
+    :return: Вернуться, Предыдущая страница, Следующая страница
+    """
     keyboard = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Вернуться', callback_data='quit')
     btn2 = types.InlineKeyboardButton('Предыдущая страница', callback_data='back')
     btn3 = types.InlineKeyboardButton('Cледующая страница', callback_data='next')
     keyboard.add(btn1, btn2, btn3)
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Заявки, страница ', reply_markup=keyboard)
+    global key3
+    key3 = keyboard
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard1()
+    bot.send_message(message.chat.id, 'Привет, я бот, можешь смотреть или создать заявку', reply_markup=key1)
+
+@bot.message_handler(func=lambda message: message.text == 'Просмотреть заявки', content_types=['text'])
+def orders(message):
+    keyboard2()
+    bot.send_message(message.chat.id, 'Заявки:', reply_markup=key2)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'quit')
+def start(call):
+    keyboard1()
+    bot.send_message(chat_id=call.message.chat.id, text='Привет, я бот, можешь смотреть или создать заявку', reply_markup=key1)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next')
+def callback_next(call):
+    keyboard3()
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Заявки, страница ', reply_markup=key3)
 
 @bot.callback_query_handler(func=lambda call: call.data == 'back')
 def callback_next(call):
     # num = num - 1
-    keyboard = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('Вернуться', callback_data='quit')
-    btn2 = types.InlineKeyboardButton('Предыдущая страница', callback_data='back')
-    btn3 = types.InlineKeyboardButton('Cледующая страница', callback_data='next')
-    keyboard.add(btn1, btn2, btn3)
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Заявки, страница ', reply_markup=keyboard)
+    keyboard3()
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Заявки, страница ', reply_markup=key3)
 
 
 
@@ -78,9 +97,8 @@ def money(message):
 
 @bot.message_handler(func=lambda message: message.text == 'Вернуться назад', content_types=['text'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(True, True)
-    keyboard.row('Просмотреть заявки', 'Создать заявку')
-    bot.send_message(message.chat.id, 'Привет, я бот, можешь смотреть или создать заявку', reply_markup=keyboard)
+    keyboard1()
+    bot.send_message(message.chat.id, 'Привет, я бот, можешь смотреть или создать заявку', reply_markup=key1)
 
 # bot.send_message()
 
